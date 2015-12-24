02f6846db3114a6da62f7cc8994ae9325370abf4
mickael <micka.dossantos.95@gmail.com>
2015-12-24 17:19:45 +0100

    optimising img_putline.c

--- img_putline.c
+++ img_putline.c
@@ -12,39 +12,44 @@
 
 #include "fdf.h"
 
-/*
-**y = a * x + p;
-*/
-
 void	img_putline(t_par *par, t_pnt a, t_pnt b)
 {
 	double	coef;
-	double	p;
 
 	coef = (b.y - a.y) / (b.x - a.x);
-	p = a.y - (coef * a.x);
 	if (-1 <= coef && coef <= 1)
 	{
 		while ((int)a.x != (int)b.x)
 		{
-			a.y = a.x * coef + p;
 			img_put_pixel(par, a, *a.color);
 			if (a.x < b.x)
+			{
+				a.y += coef;
 				a.x++;
+			}
 			else
+			{
+				a.y -= coef;
 				a.x--;
+			}
 		}
 	}
 	else
 	{
+		coef = (b.x - a.x) / (b.y - a.y);
 		while ((int)a.y != (int)b.y)
 		{
-			a.x = (a.y - p) / coef;
 			img_put_pixel(par, a, *a.color);
 			if (a.y < b.y)
+			{
+				a.x += coef;
 				a.y++;
+			}
 			else
+			{
+				a.x -= coef;
 				a.y--;
+			}
 		}
 	}
 }
