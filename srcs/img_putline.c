29e4167bf430513c39debe72802789723e8c3133
Mickael DOS SANTOS <mdos-san@e2r5p17.42.fr>
2016-02-12 02:37:46 +0100

    norm

--- srcs/img_putline.c
+++ srcs/img_putline.c
@@ -6,24 +6,44 @@
 /*   By: mdos-san <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 10:58:21 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/02/12 00:20:31 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/02/12 02:33:00 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	img_putline(t_par *par, t_pnt a, t_pnt b)
+static void	exeption(t_par *par, t_pnt a, t_pnt b)
+{
+	double coef;
+
+	coef = (b.x - a.x) / (b.y - a.y);
+	while ((int)a.y != (int)b.y)
+	{
+		img_put_pixel(par, a, color_convert(a.color));
+		if (a.y < b.y)
+		{
+			a.x += coef;
+			a.y++;
+		}
+		else
+		{
+			a.x -= coef;
+			a.y--;
+		}
+	}
+}
+
+void		img_putline(t_par *par, t_pnt a, t_pnt b)
 {
 	double	coef;
-	t_color	color;
 
 	coef = (b.y - a.y) / (b.x - a.x);
-	color = color_average(a.color, b.color);
+	a.color = color_average(a.color, b.color);
 	if (-1 <= coef && coef <= 1)
 	{
 		while ((int)a.x != (int)b.x)
 		{
-			img_put_pixel(par, a, color_convert(color));
+			img_put_pixel(par, a, color_convert(a.color));
 			if (a.x < b.x)
 			{
 				a.y += coef;
@@ -37,21 +57,5 @@ void	img_putline(t_par *par, t_pnt a, t_pnt b)
 		}
 	}
 	else
-	{
-		coef = (b.x - a.x) / (b.y - a.y);
-		while ((int)a.y != (int)b.y)
-		{
-			img_put_pixel(par, a, color_convert(color));
-			if (a.y < b.y)
-			{
-				a.x += coef;
-				a.y++;
-			}
-			else
-			{
-				a.x -= coef;
-				a.y--;
-			}
-		}
-	}
+		exeption(par, a, b);
 }
